97a2dd78a15fd884d26f749b90549ce493651da7
Rekha Mundhra <rmundhra2003@yahoo.com>
2019-05-23 09:20:17 -0400

    adding sql functionality

--- src/main/java/com/example/employeedb/EmployeedbApplication.java
+++ src/main/java/com/example/employeedb/EmployeedbApplication.java
@@ -2,6 +2,7 @@ package com.example.employeedb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeedbApplication {
